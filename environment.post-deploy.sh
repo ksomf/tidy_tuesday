3c526159e22c05a104a13d39976293bcc9d07061
Kim Somfleth <kim.somfleth@internode.on.net>
2023-07-08 11:44:30 +0930

    week 26

--- environment.post-deploy.sh
+++ environment.post-deploy.sh
@@ -1,4 +1,11 @@
 #!env bash
+
+set -e
 set -o pipefail
 
-R -e "install.packages('khroma', repos='https://cloud.r-project.org')"
+R -e "install.packages('khroma'           , repos='https://cloud.r-project.org')"
+R -e "install.packages('tidyterra'        , repos='https://cloud.r-project.org')"
+R -e "install.packages('elevatr'          , repos='https://cloud.r-project.org')"
+R -e "install.packages('rnaturalearthdata', repos='https://cloud.r-project.org')"
+R -e "install.packages('ggshadow'         , repos='https://cloud.r-project.org')"
+R -e "devtools::github('ropensci/rnaturalearthhires')"
